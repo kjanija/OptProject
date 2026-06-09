d5164815a55b28d77bd325ea712de18747d8520b
Kjanija <kjanijamersimoski@gmail.com>
2026-06-09 15:05:02 +0200

    feat: cli arguments for scenario selection

--- scripts/run_headless.py
+++ scripts/run_headless.py
@@ -8,17 +8,42 @@ if _SRC.exists() and str(_SRC) not in sys.path:
 
 from optproject.runners.headless_runner import run_headless
 from optproject.runners.headless_generational import run_headless as run_headless_generational
+from optproject.scenarios.generational_scenarios import (
+    create_competitive_world,
+    create_random_escape_world,
+    create_smart_escape_world,
+    create_two_island_world,
+)
+
+
+SCENARIOS = {
+    1: ("random", create_random_escape_world),
+    2: ("smart", create_smart_escape_world),
+    3: ("two-island", create_two_island_world),
+    4: ("competitive", create_competitive_world),
+}
 
 
 def main():
-    # cli argument to choose whether to run the generational or non-generational 
-    # version of the headless runner, default is generational
     import argparse
     parser = argparse.ArgumentParser(description="Run headless simulation")
-    parser.add_argument("--no-generational", action="store_true", help="Run the non-generational version of the headless runner")
+    parser.add_argument(
+        "--mode",
+        choices=("headless", "generational"),
+        default="generational",
+        help="Choose the headless runner to execute",
+    )
+    parser.add_argument(
+        "--scenario",
+        type=int,
+        choices=tuple(SCENARIOS.keys()),
+        default=1,
+        help="Scenario to use for the generational runner: 1=random, 2=smart, 3=two-island, 4=competitive",
+    )
     args = parser.parse_args()
-    if not args.no_generational:
-        run_headless_generational()
+    if args.mode == "generational":
+        _, world_creation_fun = SCENARIOS[args.scenario]
+        run_headless_generational(world_creation_fun=world_creation_fun)
     else:
         run_headless()
 
